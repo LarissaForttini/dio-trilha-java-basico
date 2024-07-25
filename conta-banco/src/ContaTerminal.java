755878b745d88eecfee9ff4e5ca3c8b6bade4572
Larissa <larissaforttini@gmail.com>
2024-07-25 14:23:06 -0300

    Código Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -5,20 +5,20 @@
                 Scanner scanner = new Scanner(System.in);
         
                 // Pedir número da conta
-                System.out.println("Por favor, digite o número da Conta !");
+                System.out.println("Por favor, digite o número da 933Conta.");
                 int numero = scanner.nextInt();
                 scanner.nextLine();  
         
                 // Pedir número da agência
-                System.out.println("Por favor, digite o número da Agência !");
+                System.out.println("Por favor, digite o número da Agência.");
                 String agencia = scanner.nextLine();
         
                 // Pedir nome do Cliente
-                System.out.println("Por favor, digite o nome do Cliente !");
+                System.out.println("Por favor, digite seu nome.");
                 String nomeCliente = scanner.nextLine();
         
                 // Pedir saldo do Cliente
-                System.out.println("Por favor, digite o saldo !");
+                System.out.println("Por favor, digite o seu saldo");
                 double saldo = scanner.nextDouble();
         
                 // Mensagem final
